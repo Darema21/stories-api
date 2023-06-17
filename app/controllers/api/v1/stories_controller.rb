3c6bc54e4f091fd251f976a732bf1f18bde9d818
darema dzholdoshbaeva <darema.dzhold@outlook.com>
2023-06-17 10:46:23 +0800

    MP backend need to get back to finishing up the save and edit and destroy

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -4,8 +4,22 @@ class Api::V1::StoriesController < Api::V1::BaseController
     render json: @stories #Just for testing
   end
 
-  def index
-    @stories = Story.all
+  # def index
+  #   @stories = Story.all
+  # end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def create
+    @story = Story.new(params.require(:story).permit(:title, :body))
+    if @story.save
+      render :show
+    else
+      render json: { errors: @story.errors.full_messages }
+    end
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :stories, only: [ :index ]
+      resources :stories, only: %i[index show create]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
